f12b6b26b32432ea861b2840d7cc0ed1ee7828f7
Amirhossein <amir.h.nouri2000@gmail.com>
2020-12-08 10:06:25 +0330

    removeTour method added

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import './tour.scss'
 
 export default class Tour extends Component {
     render() {
--- src/components/TourList/TourList.js
+++ src/components/TourList/TourList.js
@@ -1,14 +1,24 @@
-import React, { Component } from 'react'
-import './tourlist.scss'
-import Tour from '../Tour'
+import React, { Component } from "react";
+import "./tourlist.scss";
+import Tour from "../Tour";
+import { tourData } from "../../tourData";
 
 export default class TourList extends Component {
-    render() {
-        return (
-            <div>
-                <h2>Hello from Tourlist</h2>
-                <Tour></Tour>
-            </div>
-        )
-    }
+  state = {
+    tours: tourData,
+  };
+
+  removeTour = (id) => {
+    console.log(id);
+  };
+
+  render() {
+    console.log(this.state.tours);
+    return (
+      <div>
+        <h2>Hello from Tourlist</h2>
+        <Tour></Tour>
+      </div>
+    );
+  }
 }
