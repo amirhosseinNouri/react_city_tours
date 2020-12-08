1d03a11aaac18b3167ddb40fdf2774cdf7abbeb9
Amirhossein <amir.h.nouri2000@gmail.com>
2020-12-08 10:57:42 +0330

    tour toggle info done

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -2,6 +2,16 @@ import React, { Component } from "react";
 import "./tour.scss";
 
 export default class Tour extends Component {
+  state = {
+    showInfo: false,
+  };
+
+  handleInfo = () =>{
+      this.setState({
+          showInfo : !this.state.showInfo
+      })
+  }
+
   render() {
     const { id, city, img, name, info } = this.props.tour;
     const { removeTour } = this.props;
@@ -11,9 +21,8 @@ export default class Tour extends Component {
         <div className="img-container">
           <img src={img} alt="image of the tour" />
           <span className="close-btn">
-          <i className="fas fa-window-close"></i>
+            <i className="fas fa-window-close"></i>
           </span>
-
         </div>
 
         <div className="tour-info">
@@ -21,11 +30,11 @@ export default class Tour extends Component {
           <h4>{name}</h4>
           <h5>
             info{" "}
-            <span>
+            <span onClick={this.handleInfo}>
               <i className="fas fa-caret-square-down"></i>
             </span>
           </h5>
-          <p>{info}</p>
+          {this.state.showInfo && <p>{info}</p>}
         </div>
       </article>
     );
